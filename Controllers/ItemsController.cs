f78f0e46c5383f1f1aaf4aed22af4a82380a550a
Jim Palowski and Verna Santos <verna@>
2018-04-18 09:37:09 -0700

    Troubleshoot

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+
+        [HttpGet("/items")]
+        public ActionResult Index()
+        {
+            List<Item> allItems = Item.GetAll();
+            return View(allItems);
+        }
+
+        [HttpGet("/items/new")]
+        public ActionResult CreateForm()
+        {
+            return View();
+        }
+        [HttpPost("/items")]
+        public ActionResult Create()
+        {
+          Item newItem = new Item (Request.Form["new-item"]);
+          newItem.Save();
+          List<Item> allItems = Item.GetAll();
+          return View("Index", allItems);
+        }
+
+        [HttpPost("/items/delete")]
+        public ActionResult DeleteAll()
+        {
+            Item.ClearAll();
+            return View();
+        }
+
+    }
+}
