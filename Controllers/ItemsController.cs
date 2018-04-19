dc06db05bcf834d14b7ced673868fb2da87e6946
Guest User <Guest@epicodus-10.local>
2018-04-19 13:39:01 -0700

    Fixed Routing

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -7,33 +7,29 @@ namespace ToDoList.Controllers
     public class ItemsController : Controller
     {
 
-        [HttpGet("/items")]
-        public ActionResult Index()
-        {
-            List<Item> allItems = Item.GetAll();
-            return View(allItems);
+      [HttpGet("/categories/{categoryId}/items/new")]
+      public ActionResult CreateForm(int categoryId)
+      {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Category category = Category.Find(categoryId);
+        return View(category);
+      }
+      [HttpGet("/categories/{categoryId}/items/{itemId}")]
+      public ActionResult Details(int categoryId, int itemId)
+      {
+        Item item = Item.Find(itemId);
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Category category = Category.Find(categoryId);
+        model.Add("item", item);
+        model.Add("category", category);
+        return View(item);
+      }
+        
+            [HttpGet("/items/{id}")]
+            public ActionResult Details(int id)
+            {
+              Item item = Item.Find(id);
+              return View(item);
+            }
+          }
         }
-
-        [HttpGet("/items/new")]
-        public ActionResult CreateForm()
-        {
-            return View();
-        }
-        [HttpPost("/items")]
-        public ActionResult Create()
-        {
-          Item newItem = new Item (Request.Form["new-item"]);
-          newItem.Save();
-          List<Item> allItems = Item.GetAll();
-          return View("Index", allItems);
-        }
-
-        [HttpPost("/items/delete")]
-        public ActionResult DeleteAll()
-        {
-            Item.ClearAll();
-            return View();
-        }
-
-    }
-}
--- Models/Items.cs
+++ Models/Items.cs
@@ -1,37 +1,44 @@
 using System.Collections.Generic;
-using System;
 
 namespace ToDoList.Models
 {
   public class Item
   {
     private string _description;
+    private int _id;
     private static List<Item> _instances = new List<Item> {};
 
-    public Item (string description)
+    public Item(string description)
     {
       _description = description;
+      _instances.Add(this);
+      _id = _instances.Count;
     }
+
     public string GetDescription()
     {
       return _description;
     }
+
     public void SetDescription(string newDescription)
     {
       _description = newDescription;
+    }
+    public int GetId()
+    {
+      return _id;
     }
     public static List<Item> GetAll()
     {
       return _instances;
     }
-    public void Save()
-    {
-      _instances.Add(this);
-    }
     public static void ClearAll()
     {
       _instances.Clear();
     }
-
+    public static Item Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
   }
 }
